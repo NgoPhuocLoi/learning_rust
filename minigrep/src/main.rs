8531b4da5c128c4c8be4d7086cd4d4567f8fd702
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2026-01-15 21:54:36 +0700

    Implement minigrep: reading fs::read_to_string instead

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -17,17 +17,6 @@ fn main() {
     let query = &args[1];
     let file_path = &args[2];
 
-    let path = Path::new(file_path);
-    let file = fs::File::open(path);
-
-    match file {
-        Ok(mut f) => {
-            let mut buf = String::new();
-            f.read_to_string(&mut buf).expect("Can not read the file");
-            dbg!(buf);
-        }
-        Err(_) => {
-            panic!("File not found");
-        }
-    }
+    let content = fs::read_to_string(file_path).expect("Can not read content of the file");
+    dbg!(content);
 }
