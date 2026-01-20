880ea1304c11eaf6f27c517d5eb5f916512e928c
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2026-01-20 20:56:55 +0700

    Capturing reference and ownership

--- closure_iterator_concepts/src/main.rs
+++ closure_iterator_concepts/src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 #[derive(Debug)]
 enum ShirtColor {
     Blue,
@@ -34,14 +36,7 @@ impl Inventory {
     }
 }
 
-fn closure_inferring_type() {
-    let example_closure = |x| x;
-    let x1 = example_closure("Hello");
-    println!("{x1}");
-    // let x2 = example_closure(5); => Err
-}
-
-fn main() {
+fn test_inventory() {
     let inventory = Inventory {
         shirts: vec![ShirtColor::Red, ShirtColor::Blue, ShirtColor::Red],
     };
@@ -55,6 +50,46 @@ fn main() {
     let gift_2 = inventory.giveaway(user_preference_2);
 
     println!("The gift_1 is {:?}", gift_2);
+}
 
+fn closure_inferring_type() {
+    let example_closure = |x| x;
+    let x1 = example_closure("Hello");
+    println!("{x1}");
+    // let x2 = example_closure(5); => Err
+}
+
+fn take_ownership(v: Vec<i32>) {
+    println!("Taked ownership: {v:?}");
+}
+
+fn capturing_references_and_ownership() {
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    let only_borrows = || println!("The list in closure {list:?}");
+
+    println!("Before calling closure: {list:?}");
+    // take_ownership(list); => Err since the list was borrowed by 'only_borrows'
+    only_borrows();
+    println!("After calling closure: {list:?}");
+
+    let mut list2 = vec![1, 2, 3];
+
+    let mut borrow_and_mutate = || list2.push(10);
+
+    // println!("Before calling borrow_and_mutate: {list2:?}"); => Error
+    borrow_and_mutate();
+    println!("After calling borrow_and_mutate: {list2:?}");
+
+    // force to move ownership to the new thread by using 'move' keywork
+    thread::spawn(move || println!("List2 in new thread: {list2:?}"))
+        .join()
+        .unwrap();
+}
+
+fn main() {
+    test_inventory();
     closure_inferring_type();
+    capturing_references_and_ownership();
 }
