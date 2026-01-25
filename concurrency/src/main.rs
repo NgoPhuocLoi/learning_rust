97f7aa85a2b4f3ad1713b5fcb536a5f37b5c4fa6
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2026-01-25 20:28:45 +0700

    concurrency: structure the examples

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -2,7 +2,40 @@ use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
-fn main() {
+fn message_passing() {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let s = String::from("Hi!");
+        thread::sleep(Duration::from_secs(1));
+        tx.send(s).unwrap();
+    });
+
+    let receive = rx.recv().unwrap();
+    println!("Got: {receive}");
+}
+
+fn send_and_receive_multiple_values() {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let s = String::from("Hi!");
+        thread::sleep(Duration::from_secs(1));
+        tx.send(s).unwrap();
+
+        let s = String::from("World!");
+        thread::sleep(Duration::from_secs(1));
+        tx.send(s).unwrap();
+
+        let s = String::from("12345!");
+        thread::sleep(Duration::from_secs(1));
+        tx.send(s).unwrap();
+    });
+
+    for receive in rx {
+        println!("Got: {receive}");
+    }
+}
+
+fn multiple_senders() {
     let (tx, rx) = mpsc::channel();
     let tx2 = tx.clone();
     thread::spawn(move || {
@@ -36,4 +69,10 @@ fn main() {
     for receive in rx {
         println!("Got: {receive}");
     }
+}
+
+fn main() {
+    message_passing();
+    send_and_receive_multiple_values();
+    multiple_senders();
 }
