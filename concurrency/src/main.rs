ed1cc3223498c19cf7537cc1704d2484eb060f08
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2026-01-25 20:26:22 +0700

    concurrency: multiple senders

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -4,7 +4,7 @@ use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
-
+    let tx2 = tx.clone();
     thread::spawn(move || {
         let s = String::from("Hi!");
         thread::sleep(Duration::from_secs(1));
@@ -17,6 +17,20 @@ fn main() {
         let s = String::from("12345!");
         thread::sleep(Duration::from_secs(1));
         tx.send(s).unwrap();
+    });
+
+    thread::spawn(move || {
+        let s = String::from("Hi! from another sender");
+        thread::sleep(Duration::from_secs(1));
+        tx2.send(s).unwrap();
+
+        let s = String::from("World! another sender");
+        thread::sleep(Duration::from_secs(1));
+        tx2.send(s).unwrap();
+
+        let s = String::from("12345! another sender");
+        thread::sleep(Duration::from_secs(1));
+        tx2.send(s).unwrap();
     });
 
     for receive in rx {
