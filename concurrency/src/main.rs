69310f9c789a95bcef0941b1e9f7c92772c03724
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2026-01-26 20:40:40 +0700

    concurrency: sharing states using Mutex

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,78 +1,12 @@
-use std::sync::mpsc;
-use std::thread;
-use std::time::Duration;
-
-fn message_passing() {
-    let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
-        let s = String::from("Hi!");
-        thread::sleep(Duration::from_secs(1));
-        tx.send(s).unwrap();
-    });
-
-    let receive = rx.recv().unwrap();
-    println!("Got: {receive}");
-}
-
-fn send_and_receive_multiple_values() {
-    let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
-        let s = String::from("Hi!");
-        thread::sleep(Duration::from_secs(1));
-        tx.send(s).unwrap();
-
-        let s = String::from("World!");
-        thread::sleep(Duration::from_secs(1));
-        tx.send(s).unwrap();
-
-        let s = String::from("12345!");
-        thread::sleep(Duration::from_secs(1));
-        tx.send(s).unwrap();
-    });
-
-    for receive in rx {
-        println!("Got: {receive}");
-    }
-}
-
-fn multiple_senders() {
-    let (tx, rx) = mpsc::channel();
-    let tx2 = tx.clone();
-    thread::spawn(move || {
-        let s = String::from("Hi!");
-        thread::sleep(Duration::from_secs(1));
-        tx.send(s).unwrap();
-
-        let s = String::from("World!");
-        thread::sleep(Duration::from_secs(1));
-        tx.send(s).unwrap();
-
-        let s = String::from("12345!");
-        thread::sleep(Duration::from_secs(1));
-        tx.send(s).unwrap();
-    });
-
-    thread::spawn(move || {
-        let s = String::from("Hi! from another sender");
-        thread::sleep(Duration::from_secs(1));
-        tx2.send(s).unwrap();
-
-        let s = String::from("World! another sender");
-        thread::sleep(Duration::from_secs(1));
-        tx2.send(s).unwrap();
-
-        let s = String::from("12345! another sender");
-        thread::sleep(Duration::from_secs(1));
-        tx2.send(s).unwrap();
-    });
-
-    for receive in rx {
-        println!("Got: {receive}");
-    }
-}
+use std::sync::Mutex;
 
 fn main() {
-    message_passing();
-    send_and_receive_multiple_values();
-    multiple_senders();
+    let mutex = Mutex::new(5);
+
+    {
+        let mut m = mutex.lock().unwrap();
+        *m = 6;
+    }
+
+    println!("The value of mutex is {:?}", mutex);
 }
