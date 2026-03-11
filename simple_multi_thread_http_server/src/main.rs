47b137fc4157fb9f2fff49e1d4c507b887d5fffa
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2026-03-11 20:44:26 +0700

    Selectively responding

--- simple_multi_thread_http_server/src/main.rs
+++ simple_multi_thread_http_server/src/main.rs
@@ -2,7 +2,6 @@ use std::{
     fs,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
-    path::Path,
 };
 
 fn handle_connection(mut stream: TcpStream) {
@@ -13,10 +12,17 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|l| !l.is_empty())
         .collect();
 
-    dbg!(http_requests);
+    let first_line = &http_requests[0];
 
-    let status_line = "HTTP/1.1 200 OK";
-    let content = fs::read_to_string(Path::new("hello.html")).unwrap();
+    println!("{first_line}");
+
+    let (status_line, file_name) = if first_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 400 NOT FOUND", "404.html")
+    };
+
+    let content = fs::read_to_string(file_name).unwrap();
     let response = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
         status_line,
