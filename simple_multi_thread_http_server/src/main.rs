01ee80b40dda218601602bc6cf42fd422b089574
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2026-03-11 20:13:33 +0700

    Reading the stream with BufReader

--- simple_multi_thread_http_server/src/main.rs
+++ simple_multi_thread_http_server/src/main.rs
@@ -1,11 +1,23 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader, Read},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_connection(stream: TcpStream) {
+    let buf = BufReader::new(&stream);
+    let http_requests: Vec<String> = buf
+        .lines()
+        .map(|l| l.unwrap())
+        .take_while(|l| !l.is_empty())
+        .collect();
+
+    dbg!(http_requests);
+}
 
 fn main() {
     let listener = TcpListener::bind("localhost:9999").unwrap();
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        println!("Connection established!")
+        handle_connection(stream.unwrap());
     }
 }
