6f354c11ca34a77576bae369329d4f57fd083a22
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2026-03-12 18:40:43 +0700

    Define Worker struct to handle in ThreadPool

--- simple_multi_thread_http_server/src/lib.rs
+++ simple_multi_thread_http_server/src/lib.rs
@@ -1,18 +1,34 @@
-use std::thread;
+use std::thread::{self, JoinHandle};
+
+pub struct Worker {
+    id: usize,
+    handler: JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: usize) -> Self {
+        Self {
+            id,
+            handler: thread::spawn(|| {}),
+        }
+    }
+}
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
 
-        let threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {}
+        for id in 0..size {
+            workers.push(Worker::new(id));
+        }
 
-        Self { threads }
+        Self { workers }
     }
 
     pub fn execute<F>(&self, f: F)
